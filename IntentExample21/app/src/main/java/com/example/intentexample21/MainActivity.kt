049bcff696fcd98be57e8a1739ed6c8c5cd6a29f
Rachit Katiyar <rachitkatiyar869@gmail.com>
2023-08-08 20:12:10 +0530

    8 august

---
+++ IntentExample21/app/src/main/java/com/example/intentexample21/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.intentexample21
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rnds=(1..3).random()
+        val btn1=findViewById<ImageButton>(R.id.btn1)
+        val btn2=findViewById<ImageButton>(R.id.btn2)
+        val btn3=findViewById<ImageButton>(R.id.btn3)
+        btn1.setOnClickListener {
+            when(rnds) {
+                1 -> intent = Intent(applicationContext, MainActivity2::class.java)
+                2 -> intent = Intent(applicationContext, MainActivity3::class.java)
+                3 -> intent = Intent(applicationContext, MainActivity4::class.java)
+            }
+            startActivity(intent)
+        }
+        btn2.setOnClickListener {
+            when(rnds) {
+                1 -> intent = Intent(applicationContext, MainActivity2::class.java)
+                2 -> intent = Intent(applicationContext, MainActivity3::class.java)
+                3 -> intent = Intent(applicationContext, MainActivity4::class.java)
+            }
+            startActivity(intent)
+        }
+        btn3.setOnClickListener {
+            when(rnds) {
+                1 -> intent = Intent(applicationContext, MainActivity2::class.java)
+                2 -> intent = Intent(applicationContext, MainActivity3::class.java)
+                3 -> intent = Intent(applicationContext, MainActivity4::class.java)
+            }
+            startActivity(intent)
+        }
+    }
+}
